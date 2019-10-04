b075819d279fbd635c9fa421875744dd566242c4
Austin Goetz <a.goetz09@gmail.com>
2019-10-04 10:03:49 -0600

    added movie model

---
+++ MovieDBAssessment/MovieDBAssessment/Models/Movie.swift
@@ -0,0 +1,30 @@
+//
+//  Movie.swift
+//  MovieDBAssessment
+//
+//  Created by Austin Goetz on 10/4/19.
+//  Copyright © 2019 Austin Goetz. All rights reserved.
+//
+
+import Foundation
+
+struct TopLevelDictionary: Decodable {
+    
+    let results: [Movies]
+    
+}
+
+struct Movies: Decodable {
+    
+    let image: String //Change with keys
+    let title: String
+    let overview: String
+    let rating: Double //Change with keys
+    
+    enum CodingKeys: String, CodingKey {
+        case image = "poster_path"
+        case title
+        case overview
+        case rating = "vote_average"
+    }
+}
